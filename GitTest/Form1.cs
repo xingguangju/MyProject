0018710f28315ecbaf63fc5e676c76d178e00a13
太白 <taibai@xingguangju.com>
2014-03-20 09:19:24 +0800

    ADadsADS

--- GitTest/Form1.cs
+++ GitTest/Form1.cs
@@ -16,5 +16,10 @@ namespace GitTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("ssss")；
+        }
     }
 }
